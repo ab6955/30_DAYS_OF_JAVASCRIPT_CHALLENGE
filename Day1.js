cec26b6d6365d926ee6b6d40aeb176d3a66d2139
Abdullah Ansari <143590602+ab6955@users.noreply.github.com>
2024-07-13 12:21:53 +0000

    DAY 1: JavaScript Variables and Data Types

---
+++ Day1.js
@@ -0,0 +1,44 @@
+// Activity 1
+// Task 1:
+var task1 = 45;
+console.log(task1);
+
+// Task 2:
+let task2 = "Hitesh Chaudhary";
+console.log(task2);
+
+// Activity 2
+// Task 3
+const task3 = true;
+console.log(task3);
+
+// Activity 3
+// task4
+let name = "Hitesh sir";
+let age = 18;
+let isLoggedIn = true;
+const Heroes = ["Ironman", "superman"];
+const user = {
+  name: "SRK",
+  age: 50,
+  isLoggedIn: false,
+};
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof Heroes);
+console.log(typeof user);
+
+// Activity 4
+// task5
+let task5 = 234;
+console.log(task5);
+task5 = 334;
+console.log(task5);
+
+// Activity 5
+// task6
+const gender = "Male";
+console.log(gender);
+gender = "female"; // got an error which state =>Assignment to constant variable.
