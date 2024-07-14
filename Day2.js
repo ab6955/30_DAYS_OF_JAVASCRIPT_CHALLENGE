428c379e74bf9cc58b9da71aca6ceb699226a0c8
Abdullah Ansari <143590602+ab6955@users.noreply.github.com>
2024-07-14 17:28:59 +0000

    Day 2: Operators

--- Day2.js
+++ Day2.js
@@ -3,88 +3,90 @@
 let num1 = 2;
 let num2 = 4;
 const sum = num1 + num2;
-// console.log(sum);
+console.log(sum);
 
 // task2
 let num3 = 43;
 let num4 = 19;
 const subtract = num3 - num4;
-// console.log(subtract);
+console.log(subtract);
 
 // task3
 let num5 = 3;
 let num6 = 9;
 const multiply = num5 * num6;
-// console.log(multiply);
+console.log(multiply);
 
 // task4
 let num7 = 30;
 let num8 = 5;
 const divide = num7 / num8;
-// console.log(divide);
+console.log(divide);
 
 // task5
 let num9 = 21;
 let num10 = 2;
 const remainder = num9 % num10;
-// console.log(remainder);
+console.log(remainder);
 
 // Activity 6
 // task6
 let number1 = 12;
 number1 += 1;
-// console.log(number1);
+console.log(number1);
 
 // task7
 let number2 = 12;
 number2 -= 1;
-// console.log(number2);
+console.log(number2);
 
 // Activity 3
 // task8
 let xi = 14;
 let y1 = 24;
 if (xi > y1) {
-  //   console.log(`${xi} is greater than ${y1}`);
+  console.log(`${xi} is greater than ${y1}`);
 } else if (xi < y1) {
-  //   console.log(`${y1} is greater than ${xi}`);
+  console.log(`${y1} is greater than ${xi}`);
 }
 
 // task9
 let x2 = 24;
 let y2 = 24;
-// if (x2 >= y2) {
-//   console.log(`${x2} is greater than ${y2}`);
-// } else if (x2 < y2) {
-//   console.log(`${y2} is greater than ${x2}`);
-// }
+if (x2 >= y2) {
+  console.log(`${x2} is greater than ${y2}`);
+} else if (x2 < y2) {
+  console.log(`${y2} is greater than ${x2}`);
+}
 
 // task10
 const age1 = 20;
 const age2 = "20";
-// console.log(age1 == age2);
-// console.log(age1 === age2);
+console.log(age1 == age2);
+console.log(age1 === age2);
 
 // Activity 4
 // task 11
 let n1 = 20;
 let n2 = 30;
 if (n1 > 10 && n2 > 20) {
-  // console.log(`${n1} is greater than 10 and ${n2} is greater than 20`);
+  console.log(`${n1} is greater than 10 and ${n2} is greater than 20`);
 }
 
 // task 12
 let n3 = 50;
 let n4 = 100;
 if (n3 > 10 || n4 < 20) {
-  // console.log(`${n3} is greater than 10`);
+  console.log(`${n3} is greater than 10`);
 }
 
 // task 13
-if(!false){
-// console.log("Hitesh Chaudhary");
+if (!false) {
+  console.log("Hitesh Chaudhary");
 }
 
 // task 14
-let newNumber=23
-newNumber>0 ?console.log(`${newNumber} is Positive`):console.log(`${newNumber} is negative`)
+let newNumber = 23;
+newNumber > 0
+  ? console.log(`${newNumber} is Positive`)
+  : console.log(`${newNumber} is negative`);
